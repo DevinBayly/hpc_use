f07bc6e55a926f277b1d6c8c87fc42aad9ebb02f
Devin Bayly <baylyd@email.arizona.edu>
2024-02-26 17:44:51 +0000

    cleaning up test_custom code

--- test_custom.py
+++ test_custom.py
@@ -53,30 +53,12 @@ You are encouraged to full screen the tree map below.
 print("running example from test_custom")
 res = example([list(concept_names),list(concept_parents)],key="fixed")
 
-st.markdown(res)
-df = pd.read_csv("works.csv")
-inds = df.concepts.str.contains(str(res),na=False)
-# print(inds)
-sub = df[inds].head(n=20)
-st.table(sub)
-# pass the labels and parents
-
-# Create an instance of our component with a constant `name` arg, and
-# print its output value.
-# selected = my_component([labels,parents],key="fixed")
-# st.markdown(selected)
-
-# st.markdown("---")
-# st.subheader("Component with variable args")
-
-# Create a second instance of our component whose `name` arg will vary
-# based on a text_input widget.
-#
-# We use the special "key" argument to assign a fixed identity to this
-# component instance. By default, when a component's arguments change,
-# it is considered a new instance and will be re-mounted on the frontend
-# and lose its current state. In this case, we want to vary the component's
-# "name" argument without having it get recreated.
-# name_input = st.text_input("Enter a name", value="Streamlit")
-# num_clicks = my_component(name_input,key="foo")
-# st.markdown("You've clicked %s times!" % int(num_clicks))
+if res and 'finished' not in res:
+    st.markdown(res)
+    df = pd.read_csv("works.csv")
+    print(df.shape,res)
+    inds = df.concepts.str.contains(str(res),na=False)
+    # print(inds)
+    sub = df[inds].head(n=20)
+    print(sub)
+    st.table(sub)
